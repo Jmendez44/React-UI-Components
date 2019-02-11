ce1e2a7418dda200eff5034c052ffd32d6401aa3
jmendez44 <jenuel.mendez@gmail.com>
2019-02-11 18:47:04 -0500

    Header complete

---
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Header.css';
+
+import HeaderContent from './HeaderContent';
+import HeaderTitle from './HeaderTitle';
+import ImageThumbnail from './ImageThumbnail';
+
+function HeaderContainer() {
+    return (
+    <div className="className">
+        <ImageThumbnail />
+        <HeaderTitle />
+        <HeaderContent />
+    </div> 
+    );
+}
+
+export default HeaderContainer;
