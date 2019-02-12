c00735f0c839c43ab35bbd75969b85a9ac5303b5
jmendez44 <jenuel.mendez@gmail.com>
2019-02-12 16:24:38 -0500

    social card

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import './App.css';
 
 import HeaderContainer from './components/HeaderComponents/HeaderContainer';
-
+import CardContainer from './components/CardComponents/CardContainer';
 
 
 const App = () => {
   return (
-    <div>
+    <div className="card-container">
       <HeaderContainer />
+      <CardContainer />
     </div>
   );
 };
